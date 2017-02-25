d77e39535f8b4d3129eea8a3b998eeb52cd68dfa
Phil <phil.ficus@gmail.com>
2017-02-25 22:22:58 +0100

    Added package.json and sources

---
+++ src/sources.js
@@ -0,0 +1,268 @@
+module.exports = {
+  sources: [
+    {
+      name: 'Web',
+      items: [
+        {
+          name: 'Official Website',
+          url: 'https://news.ycombinator.com'
+        },
+        {
+          name: 'hack.ernews.info',
+          url: 'http://hack.ernews.info',
+          author: {
+            name: 'Antonis Karamitros',
+            url: 'https://github.com/AntouanK'
+          }
+        },
+        {
+          name: 'hckrnews.com',
+          url: 'http://hckrnews.com',
+          author: { name: 'Wayne Larsen', url: 'https://github.com/wvl' }
+        },
+        {
+          name: 'hackerbra.in',
+          url: 'http://hackerbra.in',
+          author: { name: 'Adrian Wajsbrem' }
+        },
+        {
+          name: 'Hacker News Daily',
+          url: 'http://www.daemonology.net/hn-daily',
+          author: { name: 'Colin Percival' }
+        },
+        {
+          name: 'Hacker News Rankings',
+          url: 'http://hnrankings.info/',
+          author: {
+            name: 'Marc Neuwirth',
+            url: 'https://github.com/marcneuwirth'
+          }
+        },
+        {
+          name: 'Hacker News Confidence',
+          url: 'http://hn.elijames.org/',
+          author: { name: 'Eli James', url: 'https://github.com/ejamesc' }
+        },
+        {
+          name: 'hnhiring.com',
+          url: 'http://hnhiring.com/',
+          author: { name: 'Jordi Noguera', url: 'https://github.com/jordinl' }
+        },
+        {
+          name: 'hnhiring.me',
+          url: 'http://hnhiring.me/',
+          author: { name: 'Micah Wylde', url: 'https://github.com/mwylde' }
+        },
+        {
+          name: 'Full Hacker News',
+          url: 'http://www.fullhn.com/',
+          author: {
+            name: 'Maurice Svay',
+            url: 'https://github.com/mauricesvay'
+          }
+        },
+        { name: 'HNWatcher', url: 'https://www.hnwatcher.com/' },
+        { name: 'HN Filter', url: 'http://hn-filter.appspot.com/' },
+        {
+          name: 'HN Hiring Mapped',
+          url: 'http://gaganpreet.github.io/hn-hiring-mapped/src/web/',
+          author: { name: 'Gaganpreet', url: 'https://github.com/gaganpreet' }
+        },
+        {
+          name: 'TiledHN',
+          url: 'http://www.tiledhn.com/',
+          author: { name: 'Prakhar Bhandari', url: 'https://github.com/pbjr23' }
+        },
+        {
+          name: 'React HN',
+          url: 'http://insin.github.io/react-hn/',
+          author: { name: 'Jonny Buchanan', url: 'https://github.com/insin' }
+        },
+        {
+          name: 'HN Paper',
+          url: 'http://hnpaper.forge.partlab.io/',
+          author: { name: 'Régis Gaidot', url: 'https://github.com/rgaidot' }
+        },
+        {
+          name: 'hn-reader',
+          url: 'http://chancancode.github.io/hn-reader',
+          author: {
+            name: 'Godfrey Chan',
+            url: 'https://github.com/chancancode'
+          }
+        },
+        {
+          name: 'serializer.io',
+          url: 'http://www.serializer.io',
+          author: {
+            name: 'Charlie Egan',
+            url: 'https://github.com/charlieegan3'
+          }
+        },
+        {
+          name: 'Wayback HN',
+          url: 'http://www.waybackhn.com/',
+          author: { name: 'Jonathan Dubin', url: 'https://github.com/jondubin' }
+        },
+        {
+          name: 'How Hacker News looked when you joined',
+          url: 'http://bemmu.github.io/hncakeday/',
+          author: { name: 'Bemmu Sepponen', url: 'https://github.com/bemmu' }
+        }
+      ]
+    },
+    {
+      name: 'Mobile Web',
+      items: [
+        {
+          name: 'ihackernews.com',
+          url: 'http://ihackernews.com/',
+          author: { name: 'Ronnie Roller' }
+        },
+        {
+          name: 'hn.premii.com',
+          url: 'http://hn.premii.com/',
+          author: { name: 'Dharmesh Patel', url: 'https://github.com/premii' }
+        },
+        {
+          name: 'HackerWeb',
+          url: 'http://hackerwebapp.com/',
+          author: { name: 'Lim Chee Aun', url: 'https://github.com/cheeaun' }
+        },
+        {
+          name: 'hackernews.im',
+          url: 'http://www.hackernews.im/',
+          author: { name: 'Poly Miao', url: 'https://github.com/polyrabbit/' },
+          source: {
+            type: 'github',
+            url: 'https://github.com/polyrabbit/hacker-news-digest'
+          }
+        },
+        {
+          name: 'Ainomma',
+          url: 'http://ainomma.azurewebsites.net/',
+          author: { name: 'Adam Cooper', url: 'https://github.com/adam7' }
+        }
+      ]
+    },
+    {
+      name: 'Telegram',
+      items: [
+        {
+          name: '@hacker_news_feed',
+          url: 'https://telegram.me/hacker_news_feed',
+          author: {
+            name: 'Phil Rukin',
+            url: 'https://github.com/phil-r'
+          }
+        }
+      ]
+    },
+    {
+      name: 'Twitter',
+      items: [
+        {
+          name: '@newsycombinator',
+          url: 'https://twitter.com/newsycombinator'
+        },
+        { name: '@hackernewsbot', url: 'https://twitter.com/hackernewsbot' },
+        { name: '@newsyc20', url: 'https://twitter.com/newsyc20' },
+        { name: '@newsyc50', url: 'https://twitter.com/newsyc50' },
+        { name: '@newsyc100', url: 'https://twitter.com/newsyc100' },
+        { name: '@newsyc150', url: 'https://twitter.com/newsyc150' },
+        { name: '@newsyc200', url: 'https://twitter.com/newsyc200' },
+        { name: '@newsyc250', url: 'https://twitter.com/newsyc250' },
+        { name: '@JustShowHN', url: 'https://twitter.com/JustShowHN' },
+        { name: '@HackerNewsYCBot', url: 'https://twitter.com/HackerNewsYCBot' }
+      ]
+    },
+    {
+      name: 'RSS',
+      items: [
+        {
+          name: 'hnapp.com',
+          url: 'http://hnapp.com',
+          author: { name: 'Nikita Gazarov', url: 'https://github.com/raquo' }
+        },
+        {
+          name: 'hnrss',
+          url: 'https://edavis.github.io/hnrss',
+          author: { name: 'Eric Davis', url: 'https://github.com/edavis' }
+        }
+      ]
+    },
+    {
+      name: 'Email',
+      items: [
+        {
+          name: 'hackernewsletter.com',
+          url: 'http://www.hackernewsletter.com',
+          author: { name: 'Kale Davis', url: 'https://github.com/kale' }
+        },
+        {
+          name: 'hnnotify.com',
+          url: 'http://hnnotify.com',
+          author: { name: 'The Buzz Media' }
+        },
+        {
+          name: 'hndigest',
+          url: 'http://hndigest.wordpress.com',
+          author: { name: 'Julian Corlet' }
+        },
+        { name: 'hndigest.com', url: 'http://www.hndigest.com' },
+        {
+          name: 'hackersdaily.com',
+          url: 'http://www.hackersdaily.com',
+          author: { name: 'Mike Singer' }
+        },
+        {
+          name: 'HN Replies',
+          url: 'http://www.hnreplies.com',
+          author: {
+            name: 'Dan Grossman',
+            url: 'https://github.com/dangrossman'
+          }
+        }
+      ]
+    },
+    {
+      name: 'Desktop',
+      items: [
+        {
+          name: 'HNClient',
+          url: 'https://florian.github.io/HNClient',
+          author: {
+            name: 'Florian Hartmann',
+            url: 'https://github.com/florian'
+          }
+        }
+      ]
+    },
+    {
+      name: 'Command Line',
+      items: [
+        {
+          name: 'haxor-news',
+          url: 'https://github.com/donnemartin/haxor-news',
+          author: {
+            name: 'Donne Martin',
+            url: 'https://github.com/donnemartin'
+          }
+        },
+        {
+          name: 'pyhn',
+          url: 'https://github.com/toxinu/pyhn',
+          author: {
+            name: 'Geoffrey Lehée',
+            url: 'https://github.com/toxinu'
+          }
+        },
+        {
+          name: 'go-hn',
+          url: 'https://gitlab.com/shank/go-hn',
+          author: { name: 'Shashank Sharma', url: 'https://gitlab.com/u/shank' }
+        }
+      ]
+    }
+  ]
+};
