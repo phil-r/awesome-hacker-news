6da34232406ace0b4318958a9cc0c2ad0a248bdf
Phil Rukin <philipp@rukin.me>
2018-07-31 16:35:41 +0200

    Update Readme and add readme generation script

---
+++ src/generate-md.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const { sources } = require('./sources');
+
+let readme = `
+# awesome-hacker-news
+:newspaper: More awesome hacker news!
+
+---
+`;
+
+const authorString = author => {
+  if (!author) return '';
+  const { name, url } = author;
+  return url ? `by [${name}](${url})` : `by ${name}`;
+};
+
+sources.sort((a, b) => a.name > b.name).forEach(({ name, items }) => {
+  readme += `\n## ${name}\n\n`;
+  items.sort((a, b) => a.name > b.name).forEach(({ name, url, author }) => {
+    readme += `[${name}](${url}) ${authorString(author)}\n`;
+  });
+});
+
+fs.writeFile('./README.md', readme, err => {
+  if (err) {
+    return console.error(err);
+  }
+
+  console.log('The readme was saved!');
+});
